d6a8170b4efeff1a7daa0998f98db2532fa016cf
QWK-001 <QWK_001@163.com>
2021-11-12 16:03:44 +0800

    final -- 1.0.12

--- nwb.config.js
+++ nwb.config.js
@@ -1,10 +1,13 @@
+var path = require('path')
 module.exports = {
   type: "react-component",
   npm: {
     esModules: true,
     umd: false,
   },
-  // devServer: {
-  //   https: true
-  // }
+  webpack: {
+    aliases: {
+      react: path.resolve('./node_modules/react'),
+    },
+  },
 };
