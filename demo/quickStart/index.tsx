125dbc2d68024fa89eba5776e4e32dc7bc7ce710
myZhangDong <973849793@qq.com>
2023-09-11 11:14:35 +0800

    feat: add quick start demo

---
+++ demo/quickStart/index.tsx
@@ -0,0 +1,72 @@
+import React, { Component, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Provider,
+  Chat,
+  ConversationList,
+  useClient,
+  rootStore,
+  Button,
+} from "chatuim2";
+import "chatuim2/style.css";
+import "./index.css";
+const appKey = ""; // your appKey
+const user = ""; // your chat user ID
+const agoraToken = ""; // agora chat token (007)
+
+const ChatApp = () => {
+  const client = useClient();
+  useEffect(() => {
+    client &&
+      client
+        .open({
+          user,
+          agoraToken,
+        })
+        .then((res) => {
+          console.log("get token success", res);
+          // create a conversation
+        });
+  }, [client]);
+
+  const createConversation = () => {
+    const conversation = {
+      chatType: "singleChat", // 'singleChat' || 'groupChat'
+      conversationId: "agora", // target user id or group id
+      name: "Agora", // target user nickname or group name
+      lastMessage: {
+        type: "txt",
+        msg: "hello",
+        time: Date.now(),
+      },
+      unreadCount: 0,
+    };
+    rootStore.conversationStore.addConversation(conversation);
+  };
+  return (
+    <div className="container">
+      <div className="conversation">
+        <ConversationList />
+      </div>
+      <div className="chat">
+        <Chat />
+      </div>
+      <Button onClick={createConversation}>create conversation</Button>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(
+  document.getElementById("quickStartRoot") as Element
+).render(
+  <Provider
+    initConfig={{
+      appKey,
+    }}
+  >
+    <ChatApp />
+  </Provider>
+);
+
+// 1. npm run dev
+// 2. open http://localhost:5173/demo/quickStart/index.html
